4653ddc449a00e743b7a478ae886ca06d698f891
Starcky Molina <starckymolina@gmail.com>
2016-11-05 11:13:14 -0300

    llamada de la funcion Peliculasx
    hice la funcion de click despues que cargue a primera pelicula y lugo
    hacer el conteco de los likes llamando a la funcion Peliculasx

--- assets/js/PF.js
+++ assets/js/PF.js
@@ -6,36 +6,75 @@ $(document).ready(function(){
 
 	var peliculas = ["Attack on titan","Total Retribution","The Boondocks","Rocky","Tarzan"]
 
-	$('#si, #no').click(function(){
-		$.ajax({
-
-			url: "http://netflixroulette.net/api/api.php",
-			data:{
-				title: peliculas[$("#si").attr("data-pag")],
-			},
-			type: "GET",
-			dataType: "json",
-
-		}).done(function(json){
-			var valor = $("#si").attr("data-pag");
-			valor = parseInt(valor) + 1;
-
-			// console.log('valor: '+ valor);
-
-			$('#si').attr('data-pag',valor);
-			
-			$('#imagenes').attr("src", json.poster);
-			$('.well').html('<p>'+ 'Titulo: ' +json.show_title+'</p>'
-						   +'<p>'+ 'Diector: ' +json.director+'</p>'
-						   +'<p>'+ 'Año: ' +json.release_year+'</p>'
-						   +'<p>'+ 'Categoria: ' +json.category+'</p>');
-			
-			// console.log(json);
+	var a = $('#si');
+	a = Number("");
+	
+	var b = $('#no');
+	b = Number("");
 
 
-		});
+		function Peliculasx(){
 		
-	});
+				
+				
+				$.ajax({
+
+					url: "http://netflixroulette.net/api/api.php",
+					data:{
+						title: peliculas[$("#si").attr("data-pag")],
+					},
+					type: "GET",
+					dataType: "json",
+
+				}).done(function(json){
+						var valor = $("#si").attr("data-pag");
+						valor = parseInt(valor) + 1;
+
+						// console.log('valor: '+ valor);
+
+						$('#si').attr('data-pag',valor);
+						
+						$('#imagenes').attr("src", json.poster);
+						$('.well').html('<p>'+ 'Titulo: ' +json.show_title+'</p>'
+									   +'<p>'+ 'Diector: ' +json.director+'</p>'
+									   +'<p>'+ 'Año: ' +json.release_year+'</p>'
+									   +'<p>'+ 'Categoria: ' +json.category+'</p>');
+						
+						// console.log(json);
+						
+						// console.log("seleccon: "+ seleccion);
+						// if(seleccion == "si"){
+						// 	a += 1;
+						// }
+						
+						// else{
+						// 	b += 1;
+						// }
+						// $('.estadisticas').html("<p>"+"Cantidad de likes: " + a + "</p>" + "<p>"+"Cantidad de x: " + b + "</p>");
+					});
+		
+		};
+
+		Peliculasx();
+
+			$('#si, #no').click(function(){
+				Peliculasx();
+
+				if($(this).attr("id") == "si")
+							seleccion = "si";
+						else
+							seleccion = "no";
+
+				console.log("seleccon: "+ seleccion);
+								if(seleccion == "si"){
+									a += 1;
+								}
+								
+								else{
+									b += 1;
+								}
+								$('.estadisticas').html("<p>"+"Cantidad de likes: " + a + "</p>" + "<p>"+"Cantidad de x: " + b + "</p>");
+			});
 
 });
 
